995b883e9e362bef73f814b94f593ec71361302d
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-05-26 11:48:51 +0530

    Time: 115 ms (41.04%), Space: 36.3 MB (12.54%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -1,9 +1,10 @@
 class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         # Approach::: 
-        self_palin=False
+        self_palin=0
         mp={};
         n=len(words);
+        # Initializing the map for counting the frequencies..
         for i in range(n): 
             if words[i] not in mp:
                 mp[words[i]]=[i]
@@ -14,23 +15,23 @@ class Solution:
         for key in mp:
             if mp[key] and key[::-1] in mp and key!=key[::-1]:
                 res+=min(len(mp[key]),len(mp[key[::-1]]))*4
-                # res+= abab baba
+                
                 mp[key]=[]
                 mp[key[::-1]]=[]
 
             elif mp[key] and key==key[::-1]:
-                
+                # jab key palindrome ho and uski freq odd ho to us time pe keval 1 me hi add hogi at last me so self_palin is used to track whether the freq of current key is odd or not. If yes then we are adding contribution by reducing 1 from the frequency.... 
                 if len(mp[key])%2==1:
                     res+=(len(mp[key])-1)*2
-                    self_palin=True
+                    self_palin=2
                 else:
+                    # agar current key palindrome but uski freq even hai then this full key will contrubute in the result, so We have added full frequency *2 to the res..
                     res+=len(mp[key])*2
                 mp[key]=[]
-            print(res)
-        # print(self_palin)
-        if self_palin:
-            res+=2
-        return res
+            # print(res)
+        # Here at last, we are adding the frequency to the result...if multiple key are palindromes so the odd frequency will contribute to the result only once... not more than that.
+
+        return res+self_palin
 
         return 5
         
