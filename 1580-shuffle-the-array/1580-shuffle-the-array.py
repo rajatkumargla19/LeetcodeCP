10d59b0a28df36a52ac0c9dca542a06ef2d5fb98
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-05-30 18:05:44 +0530

    Time: 61 ms (36.67%), Space: 18 MB (41.64%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.py
+++ 1580-shuffle-the-array/1580-shuffle-the-array.py
@@ -1,10 +1,10 @@
 class Solution:
     def shuffle(self, arr: List[int], n: int) -> List[int]:
-        n = len(arr) // 2
+        # n = len(arr) // 2
         M = max(arr) + 1  # M should be larger than any number in arr
 
         # Step 1: Encode both old and new values at each index
-        for i in range(2 * n):
+        for i in range(2*n):
             # Determine from where the new value should come
             if i % 2 == 0:
                 new_val = arr[i // 2]
@@ -13,6 +13,6 @@ class Solution:
             arr[i] += (new_val % M) * M
 
         # Step 2: Decode the new values
-        for i in range(2 * n):
+        for i in range(2*n):
             arr[i] = arr[i] // M
         return arr
