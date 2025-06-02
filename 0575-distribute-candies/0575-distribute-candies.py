b0a8291f31f15a356a3376030deb7685c3592390
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-06-02 15:47:48 +0530

    Time: 22 ms (54.27%), Space: 19.6 MB (38.81%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,15 +1,15 @@
 class Solution:
     def distributeCandies(self, candyType: List[int]) -> int:
-        # return 3
-        mp={}
-        n=len(candyType)
-        for i in range(n):
-            if candyType[i] not in mp:
-                mp[candyType[i]]=1
-            else:
-                mp[candyType[i]]+=1
-        print(mp)
+        
+        # mp={}
+        # n=len(candyType)
+        # count=0
+        # for i in range(n):
+        #     if candyType[i] not in mp:
+        #         mp[candyType[i]]=1
+        #         count+=1
+            
+        # if count<n//2:
+        #     return count
         # return n//2
-        if len(mp)<n//2:
-            return len(mp)
-        return n//2
+        return min(len(set(candyType)),len(candyType)//2)
