155aafe733bb0c02d544ddbe7c6f211c564ffb8e
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-06-04 16:21:50 +0530

    Time: 19 ms (37.12%), Space: 17.7 MB (89.39%) - LeetHub

--- 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.py
@@ -1,39 +1,14 @@
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
-        
-        if numFriends==1:return word
-        max_char='a'
-        indices=[]
         n=len(word)
-        for i in range(n):
-            if word[i]>max_char:
-                max_char=word[i]
-                indices=[i]
-            elif word[i]==max_char:
-                indices.append(i)
-        # print(max_char)
-        # print(indices)
+        if numFriends==1:return word
+        maxLength=n-numFriends+1
         res=''
-        for i in range(len(indices)):
-            temp=None
-            numF=numFriends
-            if indices[i]+1>=numF:
-                res=max(res,word[indices[i]:])
-            else:
-                numF-=indices[i]
-                # temp=word[indices[i]:n-numF+1]
-                res=max(res,word[indices[i]:n-numF+1])
-                # [3:8] abc(defgh)ij
-            # print(temp)
-            # res=max(res,temp)
+        for i in range(n):
+            # if i+maxLength>n:
+            #     res=max(res,word[i:])
+            # else:
+            #     res=max(res,word[i:i+maxLength])
+            res=max(res,word[i:]) if i+maxLength>n else max(res,word[i:i+maxLength])
         return res
-
-
-
-
-
-        
-
-
-
         
