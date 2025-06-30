86b5b6cf219ed49c13a279fd577e26444719cb83
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-06-30 13:38:16 +0530

    Time: 48 ms (29.39%), Space: 19 MB (85.83%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        n=len(nums)
+        freq={}
+        for i in range(n):
+            if nums[i] not in freq:
+                freq[nums[i]]=1
+            else:
+                freq[nums[i]]+=1
+        res=0
+        for i in freq:
+            maxx=max( (freq[i-1] if i-1 in freq else 0),(freq[i+1] if i+1 in freq else 0) )
+            res=max(res, freq[i]+maxx if maxx else 0 )
+        return res
+        
