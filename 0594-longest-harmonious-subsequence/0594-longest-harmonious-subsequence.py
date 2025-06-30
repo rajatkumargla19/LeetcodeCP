94da18e816e32d5e88277e084be4174f066ca2c3
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-06-30 13:39:34 +0530

    Time: 49 ms (28.33%), Space: 19 MB (85.83%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -9,7 +9,7 @@ class Solution:
                 freq[nums[i]]+=1
         res=0
         for i in freq:
-            maxx=max( (freq[i-1] if i-1 in freq else 0),(freq[i+1] if i+1 in freq else 0) )
-            res=max(res, freq[i]+maxx if maxx else 0 )
+            predecessorORsuccessor=max( (freq[i-1] if i-1 in freq else 0),(freq[i+1] if i+1 in freq else 0) )
+            res=max(res, freq[i]+predecessorORsuccessor if predecessorORsuccessor else 0 )
         return res
         
