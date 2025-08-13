0c1fe4c5983a4940536bad0c28aec55f9d9a001c
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-08-13 08:30:29 +0530

    Time: 6 ms (76.94%), Space: 17.8 MB (43.46%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -1,9 +1,4 @@
-import math
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n<=0:return False
-        temp=math.log(n,3)
-        temp = round(temp, 10)
-
-        print(temp)
-        return True if temp==int(temp) else False
+        largest=3**19
+        return True if (n>0 and largest%n==0) else False
