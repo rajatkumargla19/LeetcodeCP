20f8218d66f2dc1526135fa472577f5f778237e4
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-08-28 11:42:20 +0530

    Time: 11 ms (68.33%), Space: 17.8 MB (82.92%) - LeetHub

--- 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
+++ 3748-sort-matrix-by-diagonals/3748-sort-matrix-by-diagonals.py
@@ -5,27 +5,31 @@ class Solution:
         for j in range(1,n):
             row,col=0,j
             temp=[]
+            # while loop for extracting elements of each diagonal and storing them into temp for sorting purposes
             while row<m and col<n:
                 temp.append(grid[row][col])
                 row,col=row+1,col+1
             temp.sort()
-            print(temp)
+            # print(temp)
             row,col=0,j
+            # while loop for placing the extracted elements back into the diagonal of the grid
             while row<m and col<n:
                 grid[row][col]=temp[row]
                 row,col=row+1,col+1
-        # now the next symmetric code comes
-        for i in range(m):
+        #Symmetric code for placing the elements of the lower matrix elements back to the grid
+        for i in range(0,m):
             row,col=i,0
             temp=[]
+            # while loop for extracting the lower half one by one 
             while row<m and col<n:
                 temp.append(grid[row][col])
                 row,col=row+1,col+1
             temp=sorted(temp)[::-1]
             row,col=i,0
-            print(temp)
+            # print(temp)
+            # while loop for putting the extracting elements back to the grid again
             while row<m and col<n:
-                grid[row][col]=temp[col]
+                grid[row][col]=temp[col];
                 row,col=row+1,col+1
         return grid
 
