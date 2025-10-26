cf81f3eb3a9ab137278eee462922f600b87019e6
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-10-26 11:25:33 +0530

    Time: 228 ms (0%), Space: 32.2 MB (100%) - LeetHub

--- 4078-maximum-alternating-sum-of-squares/4078-maximum-alternating-sum-of-squares.py
+++ 4078-maximum-alternating-sum-of-squares/4078-maximum-alternating-sum-of-squares.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        # nums.sort()
         n=len(nums)
         for i in range(n):
             nums[i]=abs(nums[i])
         nums.sort(reverse=True)
-        print(nums)
+        # print(nums)
         half=(len(nums)+1)//2
-        print(half)
+        # print(half)
         res=0
         i=0
-
         while i<n:
-            if i<half:
-                res+=nums[i]**2
-            else:
-                res-=nums[i]**2
+            # if i<half:
+            #     res+=nums[i]**2
+            # else:
+            #     res-=nums[i]**2
+            res=(res+nums[i]**2) if i<half else (res-nums[i]**2)
             i+=1
         return res
