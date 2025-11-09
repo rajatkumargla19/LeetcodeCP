91e03f419405f0075fb5cfcafadab40e53d6d797
RAJAT KUMAR <rajat.careers21@gmail.com>
2025-11-09 17:44:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (81.03%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -6,13 +6,20 @@ class Solution:
         # (0,1)
         # return 2
         res=0
-        while num1!=0 and num2!=0:
+        if num1==0 or num2==0:return 0
+        if num1>=num2 and num1%num2==0:return num1//num2
+        if num2>num1 and num2%num1==0:return num2//num1
+        while num1 and num2:
             if num1>=num2:
-                num1-=num2
+                res+=num1//num2
+                num1=num1%num2
             else:
-                num2-=num1
-            res+=1
+                res+=num2//num1
+                num2=num2%num1
+            # print(num1,num2)
         return res
+       
+
 
 
         
