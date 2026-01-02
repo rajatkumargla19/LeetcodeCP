43a467d7269a2ef64165e819d21ff9e1a814d7c4
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-01-02 18:11:29 +0530

    Time: 3 ms (21.59%), Space: 17.3 MB (71.25%) - LeetHub

---
+++ 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumCost(self, cost1: int, cost2: int, costBoth: int, need1: int, need2: int) -> int:
+        res=0
+        if (cost1+cost2>costBoth):
+            res+=min(need1,need2)*costBoth
+            if need1>need2:
+                res+=(need1-need2)*min(cost1,costBoth)
+            else:res+=(need2-need1)*min(cost2,costBoth)
+        else:
+            res+=need1*cost1+need2*cost2
+        return res
