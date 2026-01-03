847e5e19d38a49b1200972e14f63e057b4634933
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-01-03 21:01:30 +0530

    Time: 87 ms (95.92%), Space: 17.3 MB (58.66%) - LeetHub

---
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minAllOneMultiple(self, k: int) -> int:
+        if k%2==0 or k%5==0:return -1
+        rem=0
+        for i in range(1,k+1):
+            rem=(rem*10+1)%k
+            if not(rem):return i
+        return -1
