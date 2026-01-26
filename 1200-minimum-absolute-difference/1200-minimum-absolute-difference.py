17823164cba4f88fdaef0b023e869b62ff42ce51
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-01-26 09:36:48 +0530

    Time: 51 ms (65.99%), Space: 31.4 MB (29.6%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -3,11 +3,20 @@ class Solution:
         arr.sort()
         n=len(arr)
         res=[]
-        min_diff=float('inf')
+        min_diff=arr[1]-arr[0]
         for i in range(1,n):
-            min_diff=min(min_diff,arr[i]-arr[i-1])
-        for i in range(1,n):
-            if arr[i]-arr[i-1]==min_diff:
-                res.append([arr[i-1],arr[i]])
+            # min_diff=min(min_diff,arr[i]-arr[i-1])
+            # print(min_diff,res)
+            if not(res):res.append([arr[i-1],arr[i]])
+            else:
+                if (arr[i]-arr[i-1])<min_diff:
+                    min_diff=min(min_diff,arr[i]-arr[i-1])
+                    res=[]; 
+                    res.append( [arr[i-1],arr[i]] )
+                elif (arr[i]-arr[i-1])==min_diff:
+                    res.append([arr[i-1],arr[i]])
+        # for i in range(1,n):
+        #     if arr[i]-arr[i-1]==min_diff:
+        #         res.append([arr[i-1],arr[i]])
         return res
         
