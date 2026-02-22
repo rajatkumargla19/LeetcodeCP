0a2e265d031f5b135dcc2695f9f4a78bcf32ca3c
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-02-22 11:26:04 +0530

    Time: 0 ms (100%), Space: 19.3 MB (55.21%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.py
+++ 0868-binary-gap/0868-binary-gap.py
@@ -1,22 +1,19 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
         res=0
-        # 10110
         first=0
         second=0
         i=1
         while n:
             if n%2:
-                if not(first):
-                    first=i #2
-                elif not(second):
-                    second=i #3
+                if not(first):first=i
+                elif not(second):second=i
                 else:
                     first=second
                     second=i
             res=max(res,second-first) if (first and second) else res
             n//=2
             i+=1
-        res=max(res,second-first) if (first and second) else res
-        return res
+        # res=max(res,second-first) if (first and second) else res
+        return max(res,second-first) if (first and second) else res
         
