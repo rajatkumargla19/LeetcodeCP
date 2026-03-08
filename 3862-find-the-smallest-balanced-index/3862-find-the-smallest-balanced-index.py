b5baecd6fd18855b20c36bbb8ea00297b8de1591
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-03-08 16:31:08 +0530

    Time: 134 ms (81.48%), Space: 33.3 MB (28.91%) - LeetHub

--- 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.py
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.py
@@ -6,7 +6,7 @@ class Solution:
             prefix_sum.append(prefix_sum[-1]+nums[i])
         prod=1
         for i in range(n-1,0,-1):
-            if prefix_sum[i-1]==prod:res=i
+            if prefix_sum[i-1]==prod:return i
             if prefix_sum[n-1]<prod:break
             prod*=nums[i]
         return res
