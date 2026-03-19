15947a70b6b4e66a792dff467814c2eeff14f796
RAJAT KUMAR <rajat.careers21@gmail.com>
2026-03-19 20:46:45 +0530

    Time: 362 ms (40.64%), Space: 67.1 MB (30.32%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -4,20 +4,20 @@ class Solution:
         col=len(grid[0])
         mat=[[0 for i in range(col)] for j in range(row)]
         count=0
+        # colm wise addition
         for i in range(col):
             for j in range(row):
                 mat[j][i]=(mat[j-1][i] if j>0 else 0)+grid[j][i]
-        # print(mat)
+        # now row-wise addition
         for i in range(row):
             for j in range(col):
-                if j==0:continue
-                else:
+                if j>0:
                     mat[i][j]+=mat[i][j-1]
-        # print(mat)
-        for i in range(row):
-            for j in range(col):
-                if mat[i][j]<=k:
-                    count+=1
+                if mat[i][j]<=k:count+=1
+        # for i in range(row):
+        #     for j in range(col):
+        #         if mat[i][j]<=k:
+        #             count+=1
         return count
 
 
